a6f793b33b6e9caf7505156defab909da06dcce6
Rafael Paes <rafaelpaes.al@gmail.com>
2022-11-10 13:25:47 -0300

    Revert "feat: added dynamic HTML"
    This reverts commit 1d7af9885ce144d1c65e3333a7b0a6e51d62d42d.

--- indexeddb/app.js
+++ indexeddb/app.js
@@ -3,7 +3,7 @@ import Dexie from "https://cdn.jsdelivr.net/npm/dexie@3.0.3/dist/dexie.mjs";
 const db = new Dexie("pokemonDB");
 
 db.version(1).stores({
-  pokemon: "++id,name",
+  pokemon: "++id, name",
 });
 
 db.on("populate", async () => {
@@ -11,58 +11,22 @@ db.on("populate", async () => {
     {
       name: "Bulbasaur",
       picture:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
     },
     {
       name: "Charmander",
       picture:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
     },
     {
       name: "Squirtle",
       picture:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png",
     },
     {
       name: "Pikachu",
       picture:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/36.png",
     },
   ]);
 });
-
-db.open();
-
-function byChar(char) {
-  return function (poke) {
-    console.log(poke.name);
-    return poke.name.includes(char);
-  };
-}
-
-const pokemonList = await db.pokemon
-  // .where("name")
-  // .startsWithIgnoreCase("c")
-  // .filter(byChar("a"))
-  .toArray();
-
-console.log(pokemonList);
-
-const pokeHTML = pokemonList.map(toHTML).join("");
-document.body.innerHTML = pokeHTML;
-
-function toHTML(poke) {
-  return `
-    <div>
-      <div class="card" style="border-color: var(--grass);">
-        <div class="card-id" style="color: var(--grass);">${poke.id}</div>
-        <div class="card-image">
-          <img alt="${poke.name}" src="${poke.picture}">
-        </div>
-      </div>
-      <div class="card-name" style="background-color: var(--grass);">
-        ${poke.name}
-      </div>
-    </div>
-  `;
-}
